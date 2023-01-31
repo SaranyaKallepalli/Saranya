04da4444ff9ea02e74e74cf7a72d743506c3d9e8
SaranyaKallepalli <121920726+SaranyaKallepalli@users.noreply.github.com>
2023-01-31 12:00:43 +0530

    Rapid API Dad Jokes Challenge
    Complete Dad Jokes Challenge with all enhancements.

--- script.js
+++ script.js
@@ -1,40 +1,45 @@
+const temp = document.getElementById("limit");
+
 $( document ).ready(function() {
-
   var userinput=0;
+  var count=0;
+  var total =0;
+  
+  var i=1;
+  document.querySelector('#btn').style.display = 'none'; 
+  document.querySelector('#clr').style.display = 'none'; 
+  document.querySelector('#btncount').addEventListener('click',showBtn);
+ 
+  function showBtn(e) { 
+  document.querySelector('#btn').style.display = 'block'; 
+  document.querySelector('#clr').style.display = 'block'; 
+  e.preventDefault(); 
+  } 
 
-  var count=1;
-
- $('#btncount').click(function(){
-
-
-
-  userinput= $('#txtcount').val();
-
-  count=0;
-
- console.log(userinput);
-
- })
-
-  $('#btn').click(function(){
-
-    count=count+1;
-
-    if(count>=userinput){
-
-      $('#btn').prop('disabled', true);
-
-    }
-
-    console.log(count);
-
-    $.ajax({
-
+  $('#btncount').click(function (){
+    userinput= $('#txtcount').val();
+    total=userinput-1;
+    tmepInput = userinput;
+   temp.innerHTML = "<h4 style='dispaly:flex;margin-right:-685px;font-size: 19px;margin-top: -181px;'> your count is: "+tmepInput+"</h4>"
+   if(total>0)
+   {
+   document.getElementById('btncount').style.visibility= 'hidden';
+   //console.count(userinput);
+   document.getElementById('txtcount').style.visibility= 'hidden';
+   $('#btncount').prop('disabled', true);
+    console.count(userinput);
+   }
+  //  alert("Count Submitted Successfully...Press 👉Joke")          
+  //  }
+  //  else
+  //  {
+  //   alert("Please enter the value");
+  //  }
+  $.ajax({
+ 
     type: 'GET',
 
     url: 'https://dad-jokes.p.rapidapi.com/random/joke',
-
-    //data: {get_param: 'value'},
 
     dataType: 'json',
 
@@ -42,30 +47,79 @@ $( document ).ready(function() {
 
                        'content-type': "application/json",
 
-                       'X-RapidAPI-Key': '1367946f6emsha1c90732ea00fa6p1cd02ajsnb4bc2e53b73f',
-
-                       'X-RapidAPI-Host': 'dad-jokes.p.rapidapi.com'
+          
+                         'X-RapidAPI-Key': '9f7ca612b1mshc2ba703209ff753p1b16c5jsne4f89ac34cbe',
+                         'X-RapidAPI-Host': 'dad-jokes.p.rapidapi.com'
+                    
 
                     },
+
 
     success: function (data)
 
     {
-
-      //  debugger;
-
-      $('#joke1').html(data.body[0].setup);
+      $('#joke1').html(i+". "+data.body[0].setup);
 
       console.log(data.body[0].setup);
 
-       $('#joke2').html(data.body[0].punchline);
+      $('#joke2').html(data.body[0].punchline);
 
       console.log(data.body[0].punchline);
-
+    i++;
     }
 
   });
+  count=1;
+   console.log(count);
+   $('#btn').click(function (){
+     
+     count=count+1;
+    
+      if(count==userinput){
+ 
+       $('#btn').prop('disabled', true);
+ 
+       document.getElementById('btn').style.visibility= 'hidden';
+     }
+       
+    console.log(count);
+    
+    $.ajax({
+ 
+     type: 'GET',
+ 
+     url: 'https://dad-jokes.p.rapidapi.com/random/joke',
+ 
+     dataType: 'json',
+ 
+     headers: {
+ 
+                        'content-type': "application/json",
 
-  })
+                       'X-RapidAPI-Key': '9f7ca612b1mshc2ba703209ff753p1b16c5jsne4f89ac34cbe',
+
+                       'X-RapidAPI-Host': 'dad-jokes.p.rapidapi.com'
+ 
+                     },
+ 
+ 
+     success: function (data)
+ 
+     {
+       $('#joke1').html(i+". "+data.body[0].setup);
+ 
+       console.log(data.body[0].setup);
+ 
+       $('#joke2').html(data.body[0].punchline);
+ 
+       console.log(data.body[0].punchline);
+     i++;
+     }
+ 
+   });
+ });
+  });
+   
+
 
 });
